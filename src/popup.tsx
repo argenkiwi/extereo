25360f7817cd128af7eb7b8af1c93aecb4119d68
Leandro <gringoperalta@gmail.com>
2018-03-03 16:34:14 +1300

    Remove semi-colons.

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,12 +5,12 @@ import Message from './model/Message';
 import PopUp from './components/PopUp';
 
 const message$ = Observable.fromEventPattern((h: (message: Message<any>) => void) => {
-    chrome.runtime.onMessage.addListener(h);
+    chrome.runtime.onMessage.addListener(h)
 }, (h: (message: Message<any>) => void) => {
-    chrome.runtime.onMessage.removeListener(h);
-}, (message: Message<any>) => message);
+    chrome.runtime.onMessage.removeListener(h)
+}, (message: Message<any>) => message)
 
 ReactDOM.render(
     <PopUp message$={message$} />,
     document.getElementById('root')
-);
+)

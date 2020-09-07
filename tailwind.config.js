8cf4519c9675e5137c7cc4fa88a93611afa96fa3
Leandro <leandro.peralta@live.com.ar>
2020-09-07 15:53:23 +1200

    Add PostCSS and TailwindCSS dependencies.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+}

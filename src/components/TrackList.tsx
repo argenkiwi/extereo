f95fa53b59d4fd8832bb11537576025c58dc13e0
Leandro <leandro.peralta@live.com.ar>
2020-09-10 09:16:10 +1200

    Apply TailwindCSS to TrackList.

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { SortableContainer } from 'react-sortable-hoc';
 import Track from "../model/Track";
 import TrackListItem from './TrackListItem';
-import './TrackList.css';
 
 interface Props {
     tracks: Track[]
@@ -10,7 +9,7 @@ interface Props {
 }
 
 const TrackList = ({ tracks, position }: Props) => (
-    <ol className="TrackList flex-1 mt-1">
+    <ol className="flex-1 mt-1 overflow-auto list-inside p-2 bg-gray-200">
         {tracks.map((track, i) =>
             <TrackListItem
                 index={i}

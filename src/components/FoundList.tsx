ae1845a524b99731cba3f4e00588b7f537cb20fd
Leandro <leandro.peralta@live.com.ar>
2020-09-10 08:50:51 +1200

    Apply TailwindCSS to FoundList.

--- src/components/FoundList.tsx
+++ src/components/FoundList.tsx
@@ -2,12 +2,11 @@ import * as React from 'react';
 import { add } from '../service';
 import Track from '../model/Track';
 import FoundItem from './FoundItem';
-import './FoundList.css';
 
 const FoundList = ({ tracks }: { tracks: Track[] }) => (
-    <ol className="FoundList list-inside my-0 p-2 bg-gray-200">
-        <li>
-            <span>Tracks found: {tracks.length}</span>
+    <ol className="list-inside my-0 p-2 bg-gray-200">
+        <li className="flex items-center p-1">
+            <span className="flex-1">Tracks found: {tracks.length}</span>
             <button
                 disabled={!tracks.length}
                 onClick={() => add(...tracks)}

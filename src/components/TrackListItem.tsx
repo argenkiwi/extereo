a9e2aed702bc4b9c72ba4fa1c79d36a729ec6462
Leandro <gringoperalta@gmail.com>
2020-09-11 09:20:21 +1200

    Scroll to current track.

--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -3,6 +3,7 @@ import { SortableElement } from 'react-sortable-hoc';
 import { jump, remove } from '../service';
 import Track from '../model/Track';
 import Handle from './Handle';
+import { useEffect } from 'react';
 
 interface Props extends React.HTMLProps<HTMLLIElement> {
     playing: boolean;
@@ -10,22 +11,32 @@ interface Props extends React.HTMLProps<HTMLLIElement> {
     position: number;
 }
 
-const TrackListItem = ({ playing, track, position }: Props) => (
-    <li className="flex items-center p-1 hover:bg-gray-300 group">
-        <a href={track.href} target="_blank" className="flex-1 truncate">
-            {playing ?
-                <strong>{track.title}</strong> :
-                <span>{track.title}</span>
-            }
-        </a>
-        <button onClick={() => jump(position)} disabled={playing} className="ml-1 hidden group-hover:inline">
-            <i className="icon-play-circled"></i>
-        </button>
-        <button onClick={() => remove(position)} className="mx-1 hidden group-hover:inline">
-            <i className="icon-cancel-circled"></i>
-        </button>
-        <Handle />
-    </li>
-)
+const TrackListItem = ({ playing, track, position }: Props) => {
+
+    const myRef = React.useRef(null)
+
+    useEffect(() => {
+        if (playing) {
+            myRef.current.scrollIntoView()
+        }
+    }, []);
+
+    return (
+        <li ref={myRef} className="flex items-center p-1 hover:bg-gray-300 group">
+            <a href={track.href} target="_blank" className="flex-1 truncate">
+                {playing ?
+                    <strong>{track.title}</strong> :
+                    <span>{track.title}</span>}
+            </a>
+            <button onClick={() => jump(position)} disabled={playing} className="ml-1 hidden group-hover:inline">
+                <i className="icon-play-circled"></i>
+            </button>
+            <button onClick={() => remove(position)} className="mx-1 hidden group-hover:inline">
+                <i className="icon-cancel-circled"></i>
+            </button>
+            <Handle />
+        </li>
+    );
+}
 
 export default SortableElement<Props>(TrackListItem);
